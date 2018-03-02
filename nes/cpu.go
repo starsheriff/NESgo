24ff6fcc398f209960688f7a538eacd06b7e4d02
Johannes Weissmann <jo@starsheriff.eu>
2018-03-02 14:54:24 +0100

    set up dev environment

--- nes/cpu.go
+++ nes/cpu.go
@@ -1,39 +1,49 @@
 package nes
 
+import "fmt"
+
 //      - cycles
 //
 //
 
 const ClockDivider byte = 12
 
-
 type instruction struct {
-    name string
-    size byte
-    addressing_mode byte
+	name           string
+	size           byte
+	addressingMode byte
 }
 
 // table of instructions, indexed by optcode
-var instructions = [256]instruction {
-    //TODO: complete with full instruction set
-    0x00 : {"AAA", 3, 3},
-    0x01 : {"ABC", 1, 1},
+var instructions = [256]instruction{
+	//TODO: complete with full instruction set
+	0x00: {"AAA", 3, 3},
+	0x01: {"ABC", 1, 1},
 }
 
+// CPU represents the 6502 CPU of the NES system
+type CPU struct {
+	//TODO: add all cpu internals
+	regX byte
+}
 
-type Cpu struct{
-    //TODO: add all cpu internals
-    reg_x byte
+// Construct a new Cpu
+func NewCPU() CPU {
+	return CPU{}
 }
 
 // do one step
-func (c *Cpu) Step() {}
-// run indefinitly
-func (c *Cpu) Run() {}
+func (c *CPU) Step() {}
 
-func (c *Cpu) Reset() {}
-func (c *Cpu) Powerup() {}
+// run indefinitly
+func (c *CPU) Run() {}
+
+func (c *CPU) Reset()   {}
+func (c *CPU) Powerup() {}
+
+func (c *CPU) ToString() string {
+	return fmt.Sprintf("CPU: %d", c.regX)
+}
 
 // implementation of instructions
-func (c *Cpu) Instruction_AAA() {}
-
+func (c *CPU) Instruction_AAA() {}
--- nes/cpu_test.go
+++ nes/cpu_test.go
@@ -3,15 +3,13 @@ package nes
 import "testing"
 import "fmt"
 
-
-
 func TestInstructionsTable(t *testing.T) {
-    expected := instruction{"ABC", 1, 1}
-    result := instructions[1]
+	expected := instruction{"ABC", 1, 1}
+	result := instructions[1]
 
-    fmt.Println(instructions[5], result)
+	fmt.Println(instructions[5], result)
 
-    if result != expected {
-        t.Errorf("Instruction is wrong")
-    }
+	if result != expected {
+		t.Errorf("Instruction is wrong")
+	}
 }
