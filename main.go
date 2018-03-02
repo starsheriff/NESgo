d3968e33ffc8f9432c406710f2fee288e30097c5
Johannes Weissmann <jo@starsheriff.eu>
2018-03-02 13:41:55 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+
+import (
+    "fmt"
+)
+
+type Cpu struct{
+    reg_x byte;
+}
+
+// do one step
+func (c *Cpu) step() {
+}
+
+
+func main() {
+    fmt.Println("boing")
+}
---
+++ nes/cpu_test.go
@@ -0,0 +1,7 @@
+package nes
+
+import "testing"
+
+func TestXxx(t* testing.T) {
+    t.Errorf("asdf")
+}
